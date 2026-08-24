7a019446e33c89f5f3ef122c387adda39b3d9270
Gissell Aguirre Maldonado <gmaldo100@uafs.edu>
2026-08-24 09:15:28 -0500

    Submitting

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -1,9 +1,14 @@
 package prob1;
 
 public class Prob1 {
-
 	public static void main(String[] args) {
-		System.out.printf("Hello World!\n");
+		int array1[] = {2,4,8,1,9};
+		findMaxDifference(array1);
+	}
+	
+	public static void findMaxDifference(int array1[]) {
+		System.out.print(array1[0]);
+		for(int i = 0; i < array1.length)
 
 	}
 
